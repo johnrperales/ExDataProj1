094066c8cbdece001f7ec1744b869238f9a0a68e
John R. Perales <jrpbulk@hotmail.com>
2015-01-09 16:37:07 -0600

    plot1
    Files for projc1 plot 1

---
+++ plot1.R
@@ -0,0 +1,19 @@
+##
+##
+##By Jperales
+
+#Create file handle, read subset of data
+fhandle <- file("household_power_consumption.txt")
+hpcData <- read.table(text = grep("^[1,2]/2/2007", 
+                                  readLines(fhandle), value = TRUE), 
+                      col.names = c("Date", "Time", "Global_active_power", 
+                                    "Global_reactive_power", "Voltage", "Global_intensity", 
+                                    "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+                      sep = ";", header = TRUE)
+
+# Generate Plot #1
+hist(hpcData$Global_active_power, col = "red", main = paste("Global Active Power"), xlab = "Global Active Power (kilowatts)")
+
+#Save to png file having issue here not sure why fixed working directory was not set
+dev.copy(png, file = "plot1.png")
+dev.off()
